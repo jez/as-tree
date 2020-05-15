1f986627bd98f39c846ea32f05b5063561ac595b
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-05-14 23:00:18 -0500

    Add bumping version test to the release.sh script

--- tools/scripts/release.sh
+++ tools/scripts/release.sh
@@ -12,7 +12,9 @@ fi
 sed -i.bak -e "s/version = \"[^\"]*\"/version = \"$version\"/" Cargo.toml src/BUILD
 rm Cargo.toml.bak src/BUILD.bak
 
-git add Cargo.toml src/BUILD
+bazel test --test_output=errors //test:update_cli/version/version.sh
+
+git add Cargo.toml src/BUILD test/cli/version/version.sh.exp
 
 git commit -m "Release version $version"
 git tag "$version"
