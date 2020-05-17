44d825f03540a502ed4dcd2fbe84c12691ec8f94
Jake Zimmerman <zimmerman.jake@gmail.com>
2020-05-16 23:14:52 -0500

    Generate the lockfile when bumping the version

--- tools/scripts/release.sh
+++ tools/scripts/release.sh
@@ -11,6 +11,7 @@ fi
 
 sed -i.bak -e "s/version = \"[^\"]*\"/version = \"$version\"/" Cargo.toml src/BUILD
 rm Cargo.toml.bak src/BUILD.bak
+cargo generate-lockfile --offline
 
 bazel test --test_output=errors //test:update_cli/version/version.sh
 
